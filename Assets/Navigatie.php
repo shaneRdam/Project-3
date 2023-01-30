1ae1e96dd0bb719f2800713d795467c8e64a8b48
NoahE2005 <9014121@student.zadkine.nl>
2023-01-30 14:28:38 +0100

    Navigatie in eigen php bestand

--- Assets/Navigatie.php
+++ Assets/Navigatie.php
@@ -1,10 +1,10 @@
 <html>
 <nav>
-        <a href="../Index.php">Home</a>
+        <a href="/Project-3/Index.php">Home</a>
         <br>
-        <a href="../Noah/Noah.php">Noah</a>
-        <a href="../Damien/Damien.php">Damien</a>
-        <a href="../Wiktor/Wiktor.php">Wiktor</a>
-        <a href="../Shane/Shane.php">Shane</a>
+        <a href="/Project-3/Noah/Noah.php">Noah</a>
+        <a href="/Project-3/Damien/Damien.php">Damien</a>
+        <a href="/Project-3/Wiktor/Wiktor.php">Wiktor</a>
+        <a href="/Project-3/Shane/Shane.php">Shane</a>
     </nav>
 </html>
