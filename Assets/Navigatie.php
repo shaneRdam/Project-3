b70cf10144d29dbc77f168208ede4b8c2d214a64
NoahE2005 <9014121@student.zadkine.nl>
2023-01-30 14:24:22 +0100

    Update Navigatie.php

--- Assets/Navigatie.php
+++ Assets/Navigatie.php
@@ -1,10 +1,10 @@
 <html>
 <nav>
-        <a href="Index.php">Home</a>
+        <a href="../Index.php">Home</a>
         <br>
-        <a href="Noah/Noah.php">Noah</a>
-        <a href="Damien/Damien.php">Damien</a>
-        <a href="Wiktor/Wiktor.php">Wiktor</a>
-        <a href="Shane/Shane.php">Shane</a>
+        <a href="../Noah/Noah.php">Noah</a>
+        <a href="../Damien/Damien.php">Damien</a>
+        <a href="../Wiktor/Wiktor.php">Wiktor</a>
+        <a href="../Shane/Shane.php">Shane</a>
     </nav>
 </html>
