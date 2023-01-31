fea59a7483c5ee82de24456c694ca2eeccc536f3
NoahE2005 <9014121@student.zadkine.nl>
2023-01-31 13:40:49 +0100

    a

--- Index.php
+++ Index.php
@@ -57,15 +57,99 @@
             <h2>Veelgestelde vragen</h2>
             <br>
 
-            <button class="collapsible">
+<button class="collapsible">
   <span class="icon">&#9660;</span>
-  Click me
+  Hoe registreerd je brein smaak?
 </button>
 <div class="content">
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+  <p>
+    Het merendeel van wat we proeven, komt van onze olfactorische receptoren (dat deel van ons lichaam dat een centrale rol speelt in ons gevoel van geur).
+
+    <br><br>
+
+Alles wat we doen wordt mogelijk gemaakt door retronasale geur, op die manier kunnen we smaak aan water 'toevoegen'. Onze flessen zorgen ervoor dat de geur die in onze pods zit, geregistreerd kan worden met elke slok.
+</p>
 </div>
 
 
+
+<button class="collapsible">
+  <span class="icon">&#9660;</span>
+  Wat zit er precies in de pods?
+</button>
+<div class="content">
+  <p>
+  De geur in onze pods wordt uitsluitend verkregen uit natuurlijke ingrediënten. Fruit, kruiden, specerijen. Deze geuren worden op een luchtdoorlatend materiaal geplaatst, of op wat wij graag 'het vlies' noemen.
+</p>
+</div>
+
+
+<button class="collapsible">
+  <span class="icon">&#9660;</span>
+  Waar we de ingrediënten vandaan halen?
+</button>
+<div class="content">
+  <p>
+  We kunnen natuurlijk niet al onze geheimen verklappen, maar we kunnen wél vertellen dat al onze geuren van een hoogwaardige geurfabrikant komen, die ze onttrekt aan natuurlijk bronnen. Ons geur-ontwikkelingsteam produceert daar de lekkerste smaken mee , en voilà: een geurpod is geboren.
+
+  <br><br>
+
+We zijn er trouwens heel trots op dat onze vega en vegan vrienden ook kunnen genieten van onze pods.
+</p>
+</div>
+
+
+
+<button class="collapsible">
+  <span class="icon">&#9660;</span>
+  Hoe sluit je je fles goed af?
+</button>
+<div class="content">
+  <p>
+  Onze fles heeft een ventiel aan de bovenkant van het mondstuk, waardoor je kunt blijven drinken zonder te stoppen. Houd je fles tijdens het drinken rechtop om lekken te voorkomen. Het is net alsof je met een rietje uit een glas drinkt. Draai de dop stevig dicht om lekken te voorkomen.
+</p>
+</div>
+
+
+
+<button class="collapsible">
+  <span class="icon">&#9660;</span>
+  Hoe haal je alles uit je pod?
+</button>
+<div class="content">
+  <p>
+  Je hoort luchtbelletjes als je drinkt met een geactiveerde pod. Dit is een teken dat de pod werkt!
+<br><br><br>
+1. Controleer of je je fles in elkaar hebt gezet op de manier zoals je in deze video ziet.
+<br><br>
+2. Zorg ervoor dat de pod is geactiveerd (tot aan het randje omhooggetrokken).
+<br><br>
+3. Zorg ervoor dat het rietje helemaal tot in het mondstuk is geplaatst.
+<br><br>
+Als de pod na het controleren van deze drie punten nog steeds niet werkt, neem dan de volgende twee stappen:
+<br><br>
+4. Probeer een andere pod om te checken of het aan de pod ligt of aan iets in de fles.
+<br><br>
+5. Haal de fles eerst helemaal uit elkaar, en zet 'm daarna weer ín elkaar.
+</p>
+</div>
+
+
+
+<button class="collapsible">
+  <span class="icon">&#9660;</span>
+  Wat betekenen die bubbels?
+</button>
+<div class="content">
+  <p>
+  Met onze techniek is het normaal dat er in elke slok die je neemt, kleine luchtbelletjes zitten. Deze luchtbelletjes nemen de smaak mee van de pod en stellen je brein in staat de geur als smaak te ervaren, allemaal zonder dat er iets aan je water wordt toegevoegd!
+<br><br>
+Hierbij een handige video die je tot in detail laat zien hoe het werkt.
+</p>
+</div>
+
+<br>
+<a href="../Project-3/Support/VeelgesteldeVragen.php">Alle Veelgestelde vragen ></a>
 
 
         </section>
