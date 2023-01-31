655eb542d915f805de9e5cd4d0c65ce2724f4318
NoahE2005 <9014121@student.zadkine.nl>
2023-01-31 11:09:29 +0100

    a

--- Account/action_page.php
+++ Account/action_page.php
@@ -2,12 +2,12 @@
 echo "<p>Hello world</p><br>";
 
 $Username = $_POST["uname"];
-echo $Username;
+echo $Username . "<br>";
 
 if(strlen($Username) < 5) {
-    echo "FOUT de naam is te kort";
+    echo "FOUT de naam is te kort <br><br>";
 }
 
 $Password = $_POST["password"];
-echo $Password;
+echo $Password . "<br>";
 ?>
