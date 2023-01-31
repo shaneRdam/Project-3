0007777ca6cc696e104a07ff0a3e88a4011da861
NoahE2005 <9014121@student.zadkine.nl>
2023-01-31 10:50:23 +0100

    a

---
+++ Account/App.js
@@ -0,0 +1,8 @@
+function TogglePassword() {
+    var x = document.getElementById("myInput");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
