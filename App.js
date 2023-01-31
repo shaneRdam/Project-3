a6068eff5d585b8d2c6f6145c0945d60c3ad70db
NoahE2005 <9014121@student.zadkine.nl>
2023-01-31 12:24:38 +0100

    a

--- App.js
+++ App.js
@@ -22,3 +22,14 @@ function ActiveToggle() {
         alert("Not active")
     }
 }
+
+$(document).ready(function() {
+  $('.collapse.in').prev('.panel-heading').addClass('active');
+  $('#accordion, #bs-collapse')
+    .on('show.bs.collapse', function(a) {
+      $(a.target).prev('.panel-heading').addClass('active');
+    })
+    .on('hide.bs.collapse', function(a) {
+      $(a.target).prev('.panel-heading').removeClass('active');
+    });
+});
