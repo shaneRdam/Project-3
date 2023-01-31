5a6c42ad7b2d3ee4c648d55b792a57d39b678fd4
NoahE2005 <9014121@student.zadkine.nl>
2023-01-31 10:45:22 +0100

    Revert "Delete Shane.php"
    This reverts commit 9d2a6148b4003dfee072154f0d4a2009a72389a8.

---
+++ Shane/Shane.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <a href="Index.html">Home</a>
+        <br>
+        <a href="Noah/Noah.php">Noah</a>
+        <a href="Damien/Damien.html">Damien</a>
+        <a href="Wiktor/Wiktor.html">Wiktor</a>
+        <a href="Shane/Shane.php">Shane</a>
+    </nav>
+    <br><br>
+    <form action="/action_page.php">
+            <label for="email">username :</label>
+            <input type="email" id="email" name="email"><br><br>
+            <label for="pwd">Password:</label>
+            <input type="password" id="pwd" name="pwd" minlength="8"><br><br>
+            <input type="submit">
+          </form>
+</form> 
+
+<p>If you click the "Submit" button, the form-data will be sent to a page called "/action_page.php".</p>
+
+</body>
+</html>
