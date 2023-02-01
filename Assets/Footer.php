b9238131d7bcc195947a10e76eede95f5290d3ef
NoahE2005 <9014121@student.zadkine.nl>
2023-02-01 13:32:11 +0100

    a

--- Assets/Footer.php
+++ Assets/Footer.php
@@ -3,10 +3,19 @@
         <link rel="stylesheet" href="/Project-3/Assets/StyleSheetNavigatie.css">
     </head>
     <footer>
+        <div class="FooterLinearGradient">
+            <div>
         <ul>
             <a href="/Project-3/Gezondheid/Duurzaamheid.php">Duurzaamheid</a>
             <a href="/Project-3/Gezondheid/Gezondheid.php">Gezondheid</a>
         </ul>
+        </div>
+        <div>
+
+        </div>
+        
+        </div>
+        <div class="FooterBGcolor">
         <ul class="FooterUl">
             <a>Betaling en verzending</a>
             <a>Annuleringsvoorwaarden</a>
@@ -16,5 +25,6 @@
             <a>Leveranciers info</a>
             <a>Privacy instellingen</a>
         </ul>
+        </div>
     </footer>
 </html>
--- Gezondheid/Gezondheid.php
+++ Gezondheid/Gezondheid.php
@@ -11,6 +11,8 @@
 <?php include '/Xampp/htdocs/Project-3/Assets/Navigatie.php';?>
 <div class="Maintext">
 
+<div class="GezondHeidFlexBox">
+
 <section>
     <h1>Voor je gezondheid: alleen water, niets anders</h1>
     <p>
@@ -19,40 +21,52 @@
 </section>
 <br><br><br><br>
 
-<section>
-    <h2>Safety first</h2>
-    <p>
-    Alle onderdelen van een pods up®-fles zijn gemaakt van hoogwaardige materialen. De fles is gemaakt van BPA-vrij Tritan™, dat ook wordt gebruikt voor babydrinkflessen. Het mondstuk is gemaakt van voedselveilig silicone, dat ook wordt gebruikt voor fopspenen.
-    </p>
+<section class="GezondHeidFlexBox2">
+    <img class="GezondHeidFlexBoxImg">
+    <div>
+     <h2>Safety first</h2>
+     <p>
+     Alle onderdelen van een pods up®-fles zijn gemaakt van hoogwaardige materialen. De fles is gemaakt van BPA-vrij Tritan™, dat ook wordt gebruikt voor babydrinkflessen. Het mondstuk is gemaakt van voedselveilig silicone, dat ook wordt gebruikt voor fopspenen.
+     </p>
+     </div>
     <br><br>
 </section>
-<section>
-    <h2>Water, opnieuw uitgevonden</h2>
-    <p>
-    Water is een van de gezondste dingen die we kunnen consumeren. Maar laten we eerlijk zijn, water is saai. 
-    Althans, dat was het. Het air up® drinksysteem maakt saai smaakvol, en dat zonder een ingrediënt toe te voegen. 
-    Sommigen noemen het magie, wij noemen het #ThinkNew. Proost!
-    </p>
+<section class="GezondHeidFlexBox2">
+    <img class="GezondHeidFlexBoxImg">
+    <div>
+     <h2>Water, opnieuw uitgevonden</h2>
+     <p>
+     Water is een van de gezondste dingen die we kunnen consumeren. Maar laten we eerlijk zijn, water is saai. 
+     Althans, dat was het. Het air up® drinksysteem maakt saai smaakvol, en dat zonder een ingrediënt toe te voegen. 
+     Sommigen noemen het magie, wij noemen het #ThinkNew. Proost!
+     </p>
+    </div>
     <br><br>
 </section>
-<section>
-    <h2>Geen suiker, geen onzin.</h2>
-    <p>
-    Met air up drink je echt alleen maar water. 
-    Geen suiker, geen calorieën, geen toevoegingen. 
-    Daarom zijn onze geurpods ook veilig voor bijvoorbeeld mensen met een allergie en diabetici.
-    </p>
+<section class="GezondHeidFlexBox2">
+    <img class="GezondHeidFlexBoxImg">
+    <div>
+     <h2>Geen suiker, geen onzin.</h2>
+     <p>
+     Met air up drink je echt alleen maar water. 
+     Geen suiker, geen calorieën, geen toevoegingen. 
+     Daarom zijn onze geurpods ook veilig voor bijvoorbeeld mensen met een allergie en diabetici.
+     </p>
+    </div>
     <br><br>
 </section>
-<section>
-    <h2>Natuurlijke ingrediënten</h2>
-    <p>
-    Elke geurpod bevat natuurlijke aroma's, die worden gewonnen uit nauwkeurig geselecteerde vruchten, planten en kruiden. 
-    Geen enkele smaak bestaat uit slechts één aroma. Elke pod is een unieke creatie die weergaloos smaakt.
-    </p>
+<section class="GezondHeidFlexBox2">
+    <img class="GezondHeidFlexBoxImg">
+    <div>
+     <h2>Natuurlijke ingrediënten</h2>
+     <p>
+     Elke geurpod bevat natuurlijke aroma's, die worden gewonnen uit nauwkeurig geselecteerde vruchten, planten en kruiden. 
+     Geen enkele smaak bestaat uit slechts één aroma. Elke pod is een unieke creatie die weergaloos smaakt.
+     </p>
+     </div>
     <br><br>
 </section>
-
+</div>
         </div>
 
 
