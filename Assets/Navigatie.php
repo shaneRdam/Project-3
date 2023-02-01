08fd05a695df04de5a36019380021ce1fe36a34f
NoahE2005 <9014121@student.zadkine.nl>
2023-02-01 15:42:05 +0100

    icontjes

--- Assets/Navigatie.php
+++ Assets/Navigatie.php
@@ -1,12 +1,13 @@
 <html>
     <head>
         <link rel="stylesheet" href="/Project-3/Assets/StyleSheetNavigatie.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
     <header>
         <h1>Pods up</h1>
         <div class="HeaderDiv">
-            <a href="/Project-3/Account/LogIn.php">Account</a>
-            <a>Winkelwagen</a>
+            <a href="/Project-3/Account/LogIn.php"><i class="fa fa-user Fa-Icon" style="font-size:2.5vw"></i></a>
+            <a><li class="fa fa-shopping-cart Fa-Icon" style="font-size:2.5vw"></li></a>
         </div>
     </header>
 <nav>
