506f2a1939483b1dc6d8890f6346bdd8adebfaa0
NoahE2005 <9014121@student.zadkine.nl>
2023-02-02 12:33:26 +0100

    Update action_page.php

--- Account/action_page.php
+++ Account/action_page.php
@@ -1,13 +1,13 @@
 <?php
-echo "<p>Hello world</p><br>";
+echo "Vandaag is " . date("Y/m/d") . "<br><br>";
 
 $Username = $_POST["uname"];
-echo $Username . "<br>";
+echo "Email: ".$Username . "<br>";
 
 if(strlen($Username) < 5) {
     echo "FOUT de naam is te kort <br><br>";
 }
 
 $Password = $_POST["password"];
-echo $Password . "<br>";
+echo "Password: ".$Password . "<br>";
 ?>
