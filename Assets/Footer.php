27add5b4f928e150ada1023a4c48ffec77addc3a
NoahE2005 <9014121@student.zadkine.nl>
2023-02-03 13:36:44 +0100

    a

--- Assets/Footer.php
+++ Assets/Footer.php
@@ -12,7 +12,15 @@
         </ul>
         </div>
         <div class="FooterLinearGradientPurple">
-
+            <h2>
+            Schrijf je in
+            </h2>
+            <p>
+            Schrijf je in voor de nieuwsbrief en wees als eerste op de hoogte van nieuwe smaken en exclusieve acties voor je favoriete product.
+            </p>
+            <input type="text" placeholder="Email">
+            <br><br>
+            <button class="OrangeButton">Schrijf je in</button>
         </div>
         
         </div>
