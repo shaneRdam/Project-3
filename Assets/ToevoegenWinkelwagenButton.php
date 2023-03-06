3042d4511864c863cfd923f8fb0e86aee8e68e1e
NoahE2005 <9014121@student.zadkine.nl>
2023-03-06 15:09:42 +0100

    Update ToevoegenWinkelwagenButton.php

--- Assets/ToevoegenWinkelwagenButton.php
+++ Assets/ToevoegenWinkelwagenButton.php
@@ -4,6 +4,7 @@ $ID = 0;
 
 
 <form action="/Project-3/WinkelPagina/WinkelWagen.php" method="post">
-<input type="submit" name="WinkelWagenSubmit" class="PurpleButton" value="<?php echo $ID; ?>">
-<label for="WinkelWagenSubmit">Toevoegen aan WinkelWagen</label>
+<!-- <input type="submit" name="WinkelWagenSubmit" class="PurpleButton" value="<?php echo $ID; ?>">
+<label for="WinkelWagenSubmit">Toevoegen aan WinkelWagen</label> -->
+<button type="submit" name="WinkelWagenSubmit" class="PurpleButton" value="<?php echo $ID; ?>">Toevoegen aan WinkelWagen</button>
 </form>
