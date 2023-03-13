fa7497bbebd7c2c8827e71fa4c141ae404bd24b7
shane12010 <113172905+shane12010@users.noreply.github.com>
2023-03-13 13:00:16 +0100

    WinkelWagen

--- WinkelPagina/WinkelWagen.php
+++ WinkelPagina/WinkelWagen.php
@@ -11,7 +11,29 @@
 <body>
 <?php include '../Assets/Navigatie.php';?>
 <div class="Maintext">
-
+<?php
+if (isset($_POST['add'])){
+    /// print_r($_POST['product_id']);
+    if(isset($_SESSION['cart'])){
+ 
+        if(in_array($_POST['product_id'], array_keys($_SESSION['cart']))){
+            $_SESSION['cart'][$_POST['product_id']] += 1;
+            header("location: ./");
+        }else{
+            // Create new session variable
+            $_SESSION['cart'][$_POST['product_id']] = 1;
+            // print_r($_SESSION['cart']);
+            header("location: ./");
+        }
+ 
+    }else{
+        // Create new session variable
+        $_SESSION['cart'][$_POST['product_id']] = 1;
+        // print_r($_SESSION['cart']);
+        header("location: ./");
+    }
+}
+?>
 
 
         </div>
