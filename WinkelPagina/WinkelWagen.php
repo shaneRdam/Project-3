b86c19075a296232b5a937cf286a46d73b335861
shane12010 <113172905+shane12010@users.noreply.github.com>
2023-03-13 14:18:54 +0100

    Update WinkelWagen.php

--- WinkelPagina/WinkelWagen.php
+++ WinkelPagina/WinkelWagen.php
@@ -9,31 +9,56 @@
     <title>Document</title>
 </head>
 <body>
-<?php include '../Assets/Navigatie.php';?>
-<div class="Maintext">
 <?php
-if (isset($_POST['add'])){
-    /// print_r($_POST['product_id']);
-    if(isset($_SESSION['cart'])){
- 
-        if(in_array($_POST['product_id'], array_keys($_SESSION['cart']))){
-            $_SESSION['cart'][$_POST['product_id']] += 1;
-            header("location: ./");
-        }else{
-            // Create new session variable
-            $_SESSION['cart'][$_POST['product_id']] = 1;
-            // print_r($_SESSION['cart']);
-            header("location: ./");
-        }
- 
-    }else{
-        // Create new session variable
-        $_SESSION['cart'][$_POST['product_id']] = 1;
-        // print_r($_SESSION['cart']);
-        header("location: ./");
-    }
+
+session_start();
+
+// controleer of de "add-to-cart" knop is geklikt
+if(isset($_POST["add-to-cart"])){
+  
+  // maak een array voor het item dat aan de winkelwagen wordt toegevoegd
+  $item_array = array(
+    'product_id' => $_POST["product_id"],
+    'product_name' => $_POST["product_name"],
+    'product_price' => $_POST["product_price"],
+    'product_quantity' => $_POST["product_quantity"]
+  );
+  
+  // controleer of de sessievariabele voor de winkelwagen bestaat, zo niet, maak deze dan aan
+  if(!isset($_SESSION["shopping_cart"])){
+    $_SESSION["shopping_cart"] = array();
+  }
+  
+  // voeg het item toe aan de winkelwagen
+  array_push($_SESSION["shopping_cart"], $item_array);
 }
+
 ?>
+
+<!-- HTML-formulier om een ​​item aan de winkelwagen toe te voegen -->
+<form method="post" action="">
+  <input type="hidden" name="product_id" value="1">
+  <input type="hidden" name="product_name" value="Voorbeeldproduct">
+  <input type="hidden" name="product_price" value="10.00">
+  <input type="number" name="product_quantity" value="1">
+  <input type="submit" name="add-to-cart" value="Toevoegen aan winkelwagen">
+</form>
+
+<!-- PHP-code om de items in de winkelwagen te tonen -->
+<?php
+
+// controleer of de sessievariabele voor de winkelwagen bestaat en niet leeg is
+if(isset($_SESSION["shopping_cart"]) && count($_SESSION["shopping_cart"]) > 0){
+  
+  // toon de winkelwagen-items
+  foreach($_SESSION["shopping_cart"] as $item){
+    echo $item["product_name"]." - ".$item["product_price"]." - ".$item["product_quantity"]."<br>";
+  }
+  
+}
+
+?>
+
 
 
         </div>
